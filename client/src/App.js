ea7cc35e07f1aeca533aca5cc8a6c0b46675ed7d
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-25 19:57:24 -0400

    come on

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 
 class App extends Component {
   state = { users: [] };
-
   componentDidMount() {
     fetch("/users")
       .then((res) => res.json())
@@ -13,6 +12,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
         <h1>Users</h1>
         <ul>
           {this.state.users.map((user) => (
