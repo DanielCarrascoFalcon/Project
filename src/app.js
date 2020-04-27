ac5457180d6fff9f44690764763ab163099d45ed
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-27 17:31:34 -0400

    backend ready

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+
+// Importing routes
+import taskRoutes from "./routes/tasks";
+
+// initialization
+const app = express();
+
+// Middlewares
+app.use(json());
+
+// Routes
+app.use("/tasks", taskRoutes);
+
+export default app;
