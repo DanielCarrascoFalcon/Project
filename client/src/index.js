74fa2ffe450c1ffafea651ff73a8b7760a6a67bf
Pablo Carrasco <pablo.carrasco@uc.cl>
2020-04-26 21:41:51 -0400

    first changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
+import "./assets/css/index.css";
 import App from "./App";
+import PrimerComponente from "./components/PrimerComponente";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PrimerComponente />
   </React.StrictMode>,
   document.getElementById("root")
 );
