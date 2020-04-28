7d1f209dcd8f0b10eec18b2fc69a0ef636ec2ffd
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-27 21:14:32 -0400

    ready for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import "./assets/css/App.css";
 import PrimerComponente from "./components/PrimerComponente";
 import Navbar from "./components/Navbar";
+import Home from "./views/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div>
-          <Navbar/>
-          <PrimerComponente/>
+          <Home />
         </div>
       </div>
     );
