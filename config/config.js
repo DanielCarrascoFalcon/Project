381801d252f625ee235eae6359591602ee595dea
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-29 21:10:19 -0400

    da

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const config = {
+  development: {
+    username: "project",
+    password: "project",
+    database: "postgres",
+    host: "localhost",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
+
+module.exports = config;
